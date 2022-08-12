b047ea7d2a01e9a00fcca7205af5d13f6198b9c7
Gabriel Mancilla Ducuara <gabrielmancilladucuara@gmail.com>
2022-08-11 23:03:44 -0500

    actualizated twitter , github , linkedin

--- src/App.js
+++ src/App.js
@@ -8,37 +8,34 @@ import Home from './pages/Home';
 import ProductsDetail from './pages/ProductsDetail';
 import Purchases from './pages/Purchases';
 
-
-
 function App() {
+	const isLoading = useSelector(state => state.isLoading);
+	return (
+		<HashRouter>
+			{isLoading && <LoadingScreen />}
+			<NavBar />
 
-const isLoading= useSelector(state=>state.isLoading)
-  return (
-    <HashRouter>
-      {isLoading&& <LoadingScreen/>}
-      <NavBar/>
-     
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/products/:id' element={<ProductsDetail/>}/>
-        <Route path='/purchases' element={<Purchases/>}/>
-      </Routes>
-      <footer>
-        <div className='copyright'>© Juan Manuel Blanco 2022</div>
-        <div className='social-network'>
-          <a href="https://www.linkedin.com/in/juan-manuel-blanco-vargas-646a46226/">
-             <i className="fa-brands fa-linkedin-in"></i>
-          </a>
-          <a href="https://twitter.com/juamnvb">
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-          <a href="https://github.com/jmbvdev">
-          <i className="fa-brands fa-github"></i>
-          </a>
-        </div>
-      </footer>
-    </HashRouter>
-  );
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/products/:id' element={<ProductsDetail />} />
+				<Route path='/purchases' element={<Purchases />} />
+			</Routes>
+			<footer>
+				<div className='copyright'>© Gabriel Mancilla Ducuara 2022</div>
+				<div className='social-network'>
+					<a href='https://www.linkedin.com/in/juan-manuel-blanco-vargas-646a46226/'>
+						<i className='fa-brands fa-linkedin-in'></i>
+					</a>
+					<a href='https://twitter.com/Gabriel17106111'>
+						<i className='fa-brands fa-twitter'></i>
+					</a>
+					<a href='https://github.com/gaboducuara?tab=repositories'>
+						<i className='fa-brands fa-github'></i>
+					</a>
+				</div>
+			</footer>
+		</HashRouter>
+	);
 }
 
 export default App;
